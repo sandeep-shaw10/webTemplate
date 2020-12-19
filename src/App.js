f7c447c532e75e90c8aa2318ea9b1c9595dd458c
Sandeep Shaw <68613895+sandeep-shaw10@users.noreply.github.com>
2020-12-20 00:44:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ function App(){
         <Router>                 
             <Navbar/>
             <Switch>
-                <BrowserRouter basename="/webTemplate"/>
-                <Route path="/app" exact component={Home}/>
+                <Route path="/" exact component={Home}/>
                 <Route path="/service" exact component={Service}/>
                 <Route path="/product" exact component={Product}/>
                 <Route path="/sign-up" exact component={SignUp}/>
                 <Route path="" component={ErrorPage}/>
+                <Redirect to="/" />
             </Switch>
             <Footer/>
         </Router>
